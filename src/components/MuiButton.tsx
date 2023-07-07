0b64541248c3ead3be8eb3fe6c8008f312b92723
Shasika Udayanga <shasikar@mybudget.com.au>
2023-07-07 07:48:38 +0530

    mui basics

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Stack, Button } from '@mui/material';
+import { useState } from 'react';
+
+const MuiButton = () => {
+  
+  return (
+    <Stack spacing={4} alignContent='center'>
+
+        <Stack spacing={3} direction='row'>
+            <Button variant='text'
+                onClick={() => {
+                    alert('clicked');
+                }}
+            >Text</Button>
+            <Button variant='contained'>Contained</Button>
+            <Button variant='outlined'>Outlined</Button>
+        </Stack>
+
+        <Stack spacing={3} direction='row'>
+            <Button variant='text' color='primary'>Text</Button>
+            <Button variant='contained' color='error'>Contained</Button>
+            <Button variant='outlined' color='info'>Outlined</Button>
+        </Stack>
+
+
+
+    </Stack>
+  )
+}
+
+export default MuiButton
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant='h1'>h1 heading</Typography>
+      <Typography variant='h2'>h2 heading</Typography>
+      <Typography variant='h3'>h3 heading</Typography> 
+    </div>
+  )
+}
+
+export default MuiTypography
